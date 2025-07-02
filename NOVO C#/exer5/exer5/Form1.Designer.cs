0693142038b5d06d733cb4cc390226b1b7e1fe8e
brunorudalov <brudalov@gmail.com>
2025-07-02 16:18:55 -0300

    exercio 5
    no C#

---
+++ NOVO C#/exer5/exer5/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace exer5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtlados = new System.Windows.Forms.TextBox();
+            this.CalcularBtm = new System.Windows.Forms.Button();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtlados
+            // 
+            this.txtlados.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlados.Location = new System.Drawing.Point(123, 70);
+            this.txtlados.Multiline = true;
+            this.txtlados.Name = "txtlados";
+            this.txtlados.Size = new System.Drawing.Size(151, 47);
+            this.txtlados.TabIndex = 0;
+            this.txtlados.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // CalcularBtm
+            // 
+            this.CalcularBtm.Location = new System.Drawing.Point(160, 165);
+            this.CalcularBtm.Name = "CalcularBtm";
+            this.CalcularBtm.Size = new System.Drawing.Size(92, 43);
+            this.CalcularBtm.TabIndex = 1;
+            this.CalcularBtm.Text = "Calcular";
+            this.CalcularBtm.UseVisualStyleBackColor = true;
+            this.CalcularBtm.Click += new System.EventHandler(this.CalcularBtm_Click);
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.BackColor = System.Drawing.Color.Red;
+            this.lblResul.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResul.Location = new System.Drawing.Point(165, 298);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(75, 31);
+            this.lblResul.TabIndex = 2;
+            this.lblResul.Text = "Area";
+            this.lblResul.Click += new System.EventHandler(this.lblResul_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 498);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.CalcularBtm);
+            this.Controls.Add(this.txtlados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtlados;
+        private System.Windows.Forms.Button CalcularBtm;
+        private System.Windows.Forms.Label lblResul;
+    }
+}
+
---
+++ NOVO C#/exer5/exer5/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exer5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblResul_Click(object sender, EventArgs e)
+        {
+       
+
+            
+
+        }
+
+        private void CalcularBtm_Click(object sender, EventArgs e)
+        {
+            decimal lados = Convert.ToDecimal(txtlados.Text);
+            decimal area = 0;
+            decimal areaTotal = 0;
+            decimal resultado = 0;
+
+            area = lados * lados;
+            areaTotal = area * 2;
+            resultado = areaTotal;
+            lblResul.Text = resultado.ToString();
+        }
+    }
+}
